08c5edab8ea7f8b8959fc3820c716a63ed7bcc92
Luke Horvat <lukehorvat@gmail.com>
2016-10-11 17:36:02 +1100

    NEW: Implement first draft

--- test/index.js
+++ test/index.js
@@ -1,7 +1,27 @@
-import jwtValidator from "../lib";
+import should from "should";
+import { encode as encodeJWT } from "jwt-simple";
+import validateJWT from "../lib";
+
+const appUrl = "https://example.com";
+const jwtSecret = "secret";
 
 describe("aaf-rapid-connect-jwt-validator", () => {
-  it("should X", () => {
-    // TODO
+  it("should return user attributes when JWT is valid", () => {
+    let jwt = {
+      iss: "https://rapid.aaf.edu.au",
+      aud: appUrl,
+      nbf: Math.round(Date.now() / 1000) - 1000,
+      exp: Math.round(Date.now() / 1000) + 1000,
+      jti: "abc123",
+      "https://aaf.edu.au/attributes": {
+        attr1: 1,
+        attr2: 2
+      }
+    };
+    let assertion = encodeJWT(jwt, jwtSecret);
+    let userAttributes = validateJWT(assertion, appUrl, jwtSecret);
+    userAttributes.should.be.an.Object().and.have.keys("attr1", "attr2");
+    userAttributes.attr1.should.be.a.Number().and.equal(1);
+    userAttributes.attr2.should.be.a.Number().and.equal(2);
   });
 });
