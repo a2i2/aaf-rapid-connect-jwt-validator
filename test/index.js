7e931e1d1ebd2665781f464d2ca6eaf86b754c68
Luke Horvat <lukehorvat@gmail.com>
2016-10-13 17:28:32 +1100

    QUALITY: Test a few validations

--- test/index.js
+++ test/index.js
@@ -2,23 +2,24 @@ import should from "should";
 import { encode as encodeJWT } from "jwt-simple";
 import validateJWT from "../lib";
 
-const appUrl = "https://example.com";
-const jwtSecret = "secret";
 const noop = () => {};
+const jwtSecret = "secret";
+const appUrl = "https://example.com";
+const createValidJWT = () => ({
+  iss: "https://rapid.aaf.edu.au",
+  aud: appUrl,
+  nbf: Math.round(Date.now() / 1000) - 60,
+  exp: Math.round(Date.now() / 1000) + 60,
+  jti: "abc123",
+  "https://aaf.edu.au/attributes": {
+    name: "John Smith",
+    email: "john.smith@example.com"
+  }
+});
 
 describe("aaf-rapid-connect-jwt-validator", () => {
   it("should return a Promise resolved with user attributes", () => {
-    let jwt = {
-      iss: "https://rapid.aaf.edu.au",
-      aud: appUrl,
-      nbf: Math.round(Date.now() / 1000) - 1000,
-      exp: Math.round(Date.now() / 1000) + 1000,
-      jti: "abc123",
-      "https://aaf.edu.au/attributes": {
-        attr1: 1,
-        attr2: 2
-      }
-    };
+    let jwt = createValidJWT();
 
     let promise = validateJWT({
       assertion: encodeJWT(jwt, jwtSecret),
@@ -31,9 +32,57 @@ describe("aaf-rapid-connect-jwt-validator", () => {
     promise.should.be.a.Promise();
 
     return promise.then(attrs => {
-      attrs.should.be.an.Object().and.have.keys("attr1", "attr2");
-      attrs.attr1.should.be.a.Number().and.equal(1);
-      attrs.attr2.should.be.a.Number().and.equal(2);
+      attrs.should.be.an.Object().and.have.keys("name", "email");
+      attrs.name.should.be.a.String().and.equal("John Smith");
+      attrs.email.should.be.a.String().and.equal("john.smith@example.com");
+    });
+  });
+
+  it("should throw an error when JWT issuer is invalid", done => {
+    let jwt = Object.assign(createValidJWT(), { iss: "invalid" });
+
+    validateJWT({
+      assertion: encodeJWT(jwt, jwtSecret),
+      appUrl,
+      jwtSecret,
+      findToken: noop,
+      storeToken: noop
+    }).then(() => {
+      throw new Error("Invalid JWT successfully passed validation. :(");
+    }).catch(error => {
+      done(error.message === "Invalid JWT issuer." ? null : error);
+    });
+  });
+
+  it("should throw an error when JWT audience is invalid", done => {
+    let jwt = Object.assign(createValidJWT(), { aud: "invalid" });
+
+    validateJWT({
+      assertion: encodeJWT(jwt, jwtSecret),
+      appUrl,
+      jwtSecret,
+      findToken: noop,
+      storeToken: noop
+    }).then(() => {
+      throw new Error("Invalid JWT successfully passed validation. :(");
+    }).catch(error => {
+      done(error.message === "Invalid JWT audience." ? null : error);
+    });
+  });
+
+  it("should throw an error when JWT identifier is invalid", done => {
+    let jwt = createValidJWT();
+
+    validateJWT({
+      assertion: encodeJWT(jwt, jwtSecret),
+      appUrl,
+      jwtSecret,
+      findToken: () => true,
+      storeToken: noop
+    }).then(() => {
+      throw new Error("Invalid JWT successfully passed validation. :(");
+    }).catch(error => {
+      done(error.message === "Invalid JWT identifier." ? null : error);
     });
   });
 });
